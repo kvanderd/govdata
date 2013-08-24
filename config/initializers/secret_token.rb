17aa43c456ac4245a657060dad4d52611d43e785
kvanderd <me@katharine.org>
2013-08-23 21:07:29 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Govdatacsv::Application.config.secret_token = 'e196b6b4b9b1b9b78a1853a2271a2c1b7924507fb5985b211cbc250f8735a46f8b4db6128bbf87d7d15a6fa7fbd08cb617f34a2342a3ff94384043922ca563e2'
